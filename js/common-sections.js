e853a79a49feadef7cc8ec64f4f08dca4f59bbdf
Esteban Herrera <estebanhb2@yahoo.com.mx>
2016-04-07 17:04:49 -0500

    HTML formatting and updates
    HTML formatting and updates for all chapters

--- js/common-sections.js
+++ js/common-sections.js
@@ -1,6 +1,5 @@
-
-$(document).ready(function(){
-	var nav = '<div class="container"> \
+$(document).ready(function() {
+    var nav = '<div class="container"> \
 		<div class="title pull-left"> \
 			<b>Java 8 Programmer II Study Guide</b> \
 		</div> \
@@ -44,7 +43,7 @@ $(document).ready(function(){
 				<li><a tabindex="-1" href="ch29.html">29. JDBC API</a></li> \
 				<li><a tabindex="-1" href="ch30.html">30. Localization</a></li> \
 				<li><a tabindex="-1" href="upgrade.html">Upgrade from Java 6/7</a></li> \
-				<li><a tabindex="-1" href="ch31.html">A1. From Java 6 to Java 8</a></li> \
+				<li><a tabindex="-1" href="ch31.html">A1. From Java 6/7 to Java 8</a></li> \
 			  </ul> \
 			</li> \
 		  </ul> \
@@ -90,42 +89,42 @@ $(document).ready(function(){
 				<li><a tabindex="-1" href="ch29.html">29. JDBC API</a></li> \
 				<li><a tabindex="-1" href="ch30.html">30. Localization</a></li> \
 				<li><a tabindex="-1" href="upgrade.html">Upgrade from Java 6/7</a></li> \
-				<li><a tabindex="-1" href="ch31.html">A1. From Java 6 to Java 8</a></li> \
+				<li><a tabindex="-1" href="ch31.html">A1. From Java 6/7 to Java 8</a></li> \
 			</ul> \
 		  </li> \
 		</ul> \
 	  </div>';
-	  
-	var buy_info = '<p><a href="http://www.amazon.com/dp/B01DXSGHX4" target="_blank">Buying the print/kindle version from Amazon</a></p> \
+
+    var buy_info = '<p><a href="http://www.amazon.com/dp/B01DXSGHX4" target="_blank">Buying the print/kindle version from Amazon</a></p> \
 		<p><a href="https://leanpub.com/java8-programmer-ii-study-guide/" target="_blank">Buying the PDF/EPUB/MOBI version from Leanpub</a></p>';
-	  
-	var problem_info = '<p>Do you have a problem or something to say?</p> \
+
+    var problem_info = '<p>Do you have a problem or something to say?</p> \
 		<p><a href="https://github.com/eh3rrera/ocpj8-book/issues" target="_blank">Report an issue with the book</a></p> \
 		<p><a href="http://eherrera.net/" target="_blank">Contact me</a></p>';
-	  
-	var book_info = '<p>Do you like what you read? Would you consider?</p> ' +
-		buy_info +
-		'<br/> ' +
-		problem_info;
-		
-	var footer = '<div class="container"> \
+
+    var book_info = '<p>Do you like what you read? Would you consider?</p> ' +
+        buy_info +
+        '<br/> ' +
+        problem_info;
+
+    var footer = '<div class="container"> \
 		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" rel="dct:type">work</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>. \
 	  </div>';
-	
-	$('div.nav').append(nav);
-	$('div.book-info').append(book_info);
-	$('div.buy-info').append(buy_info);
-	$('footer').append(footer);
 
-	$("a.dropdown-desktop").click(function(event){
-		showMenu($("div.menu.desktop"), event);
-	})
-	$("a.dropdown-mobile").click(function(event){
-		showMenu($("div.menu.mobile"), event);
-	})
+    $('div.nav').append(nav);
+    $('div.book-info').append(book_info);
+    $('div.buy-info').append(buy_info);
+    $('footer').append(footer);
 
-	function showMenu(el, event) {
-		event.preventDefault();
-		el.toggleClass("open");
-	}
+    $("a.dropdown-desktop").click(function(event) {
+        showMenu($("div.menu.desktop"), event);
+    })
+    $("a.dropdown-mobile").click(function(event) {
+        showMenu($("div.menu.mobile"), event);
+    })
+
+    function showMenu(el, event) {
+        event.preventDefault();
+        el.toggleClass("open");
+    }
 });
